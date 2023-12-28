7029f468d2c3931044ed57ca5cf283d3e6fdc653
Vishnu <vishnuitsrocking@gmail.com>
2023-12-28 12:54:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+// import express from 'express';
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/twitter', (req, res) => {
+  res.send('Twitter');
+});
+
+app.get('/login', (req, res) => {
+  res.send('<h1>Please login</h1>');
+});
+
+app.get('/youtube', (req, res) => {
+  res.send('<h2>Youtube</h2>');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
